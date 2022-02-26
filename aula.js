0775d2ce534081663c666c11bbfa135f3c1bdbe3
Amanda Paolla <paollafermendes@gmail.com>
2022-02-25 22:07:09 -0300

    Retomar o bloco de código 1.5.2 da apostila 02 falando sobre método MAP

--- aula.js
+++ aula.js
@@ -112,3 +112,52 @@ console.log (a == b)
 console.log(nome !== " ")
 */
 
+
+/*________________________________DECLARAÇÃO DE VETORES________________________________
+v1 = [];
+
+//podemos acessar qualquer posição, começando de zero
+v1[0] = 3.4; //variavel de referência (ponteiro)
+v1[10] = 2; 
+v1[2] = true
+
+//aqui, v1 tem comprimento igual a 11
+console.log(v1.length)
+
+//inicializando na declaração
+//v2 = [2, "abc", true]
+//console.log(v2)
+
+//iterando 
+for (let i = 0; i < v1.length; i++){
+    console.log(v1[i])
+}
+*/
+
+
+/*
+const numeros = [1, 2]
+console.log(numeros.length)
+numeros [4] = 2 
+console.log(numeros.length)
+
+//por const não vale fazer isso -> numeros = [1, 2]
+//vetores são imutaveis então numeros[100] = 2 ...
+*/
+
+
+//ENCONTRANDO NOMES
+//vetor
+const nomes = [`Ana Maria`, `Antonio`, `Rodrigo`, `Alex`, `Cristina`];
+
+/*arrow function
+() => ()
+
+function filter(funcao){
+    funcao()
+}*/
+
+//filtro 
+const apenasComA = nomes.filter((n) => n.startsWith("A"));
+console.log(apenasComA);
+
